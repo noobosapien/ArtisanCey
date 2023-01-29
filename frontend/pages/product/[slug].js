48ebd80c86c5972e92cb3b44984dd5c14e6000fe
mitnikkozo <migirulz44@gmail.com>
2023-01-29 22:38:02 +1300

    Product slug start

--- frontend/pages/product/[slug].js
+++ frontend/pages/product/[slug].js
@@ -1,8 +1,74 @@
 import React, { useContext, useEffect, useState } from 'react';
 import Layout from '../../components/Layout';
+import { AppBar, Button, Grid, Toolbar, Typography } from '@mui/material';
+import Image from 'next/image';
+import Example from '../../public/example.png';
+import { Box } from '@mui/material';
 
 export default function ProductPage(props) {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Grid
+        container
+        justifyContent="space-evenly"
+        alignItems="center"
+        sx={{ marginTop: '4rem' }}
+        spacing={10}
+      >
+        <Grid item>
+          <Image src={Example} layout="intrinsic" />
+        </Grid>
+
+        <Grid item>
+          <Grid container direction="column" alignItems="center">
+            <Grid item>
+              <Typography variant="h3">Name</Typography>
+            </Grid>
+            <Grid item>
+              <Typography>description</Typography>
+            </Grid>
+
+            <Grid item>
+              <Typography sx={{ fontSize: '2rem' }}>$9.99</Typography>
+            </Grid>
+
+            <Box
+              component={Grid}
+              item
+              sx={(theme) => ({
+                [theme.breakpoints.down('sm')]: {
+                  display: 'none',
+                },
+              })}
+            >
+              <Button
+                variant="outlined"
+                sx={{ paddingLeft: '10rem', paddingRight: '10rem' }}
+              >
+                Get it now
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <AppBar
+        position="fixed"
+        color="primary"
+        sx={(theme) => ({
+          top: 'auto',
+          bottom: 0,
+          [theme.breakpoints.up('sm')]: { display: 'none' },
+        })}
+      >
+        <Toolbar>
+          <Button fullWidth variant="contained" color="secondary">
+            Get it now
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
